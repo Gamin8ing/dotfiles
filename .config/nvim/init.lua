cfc920168fecd2e641886a4d0ceb1418c77bbd73
Gamin8ing <anim8tor.ing@gmail.com>
2025-01-30 19:41:37 +0000

    Added wezterm config folder and updated the nvim config folder

---
+++ .config/nvim/init.lua
@@ -0,0 +1,3 @@
+require("config.options") -- loading options for nvim
+require("config.keymaps") -- global keymaps to set
+require("config.lazy") -- loading lazy
---
+++ .config/wezterm/colors.lua
@@ -0,0 +1,88 @@
+-- local colors = {
+--     foreground = "#CBE0F0",
+--     background = "#011423",
+    
+--     cursor_bg = "#47FF9C",
+--     cursor_border = "#47FF9C",
+--     cursor_fg = "#011423",
+    
+--     selection_bg = "#033259",
+--     selection_fg = "#CBE0F0",
+    
+--     -- ANSI Colors
+--     black = "#214969",
+--     red = "#E52E2E",
+--     green = "#44FFB1",
+--     yellow = "#FFE073",
+--     blue = "#0FC5ED",
+--     purple = "#A277FF",
+--     light_cyan = "#24EAF7",
+--     white = "#24EAF7",
+
+--     -- Bright Colors (Matching ANSI)
+--     bright_black = "#214969",
+--     bright_red = "#E52E2E",
+--     bright_green = "#44FFB1",
+--     bright_yellow = "#FFE073",
+--     bright_blue = "#A277FF",
+--     bright_purple = "#A277FF",
+--     bright_light_cyan = "#24EAF7",
+--     bright_white = "#24EAF7",
+
+--     -- Tab Bar Colors
+--     tab_bar_bg = "rgba(0,0,0,0)",
+--     active_tab_bg = "#0FC5ED",
+--     active_tab_fg = "#011423",
+--     inactive_tab_bg = "#011729",
+--     inactive_tab_fg = "#74818A",
+-- }
+
+-- return colors
+
+local colors = {
+    foreground = "#c0caf5",
+    background = "#1a1b26",
+
+    cursor_bg = "#c0caf5",
+    cursor_border = "#c0caf5",
+    cursor_fg = "#1a1b26",
+
+    selection_bg = "#283457",
+    selection_fg = "#c0caf5",
+
+    -- ANSI Colors
+    black = "#15161e",
+    red = "#f7768e",
+    green = "#9ece6a",
+    yellow = "#e0af68",
+    blue = "#7aa2f7",
+    purple = "#bb9af7",
+    light_cyan = "#7dcfff",
+    white = "#a9b1d6",
+
+    -- Bright Colors (Matching ANSI)
+    bright_black = "#414868",
+    bright_red = "#f7768e",
+    bright_green = "#9ece6a",
+    bright_yellow = "#e0af68",
+    bright_blue = "#7aa2f7",
+    bright_purple = "#bb9af7",
+    bright_light_cyan = "#7dcfff",
+    bright_white = "#c0caf5",
+
+    -- Tab Bar Colors
+    tab_bar_bg = "rgba(0,0,0,0)",
+    active_tab_bg = "#1a1b26",
+    active_tab_fg = "#7aa2f7",
+    inactive_tab_bg = "#16161e",
+    inactive_tab_fg = "#545c7e",
+    inactive_tab_edge = "#16161e",
+    inactive_tab_hover_bg = "#16161e",
+    inactive_tab_hover_fg = "#7aa2f7",
+    new_tab_bg = "#191b28",
+    new_tab_fg = "#7aa2f7",
+    new_tab_hover_bg = "#7aa2f7",
+    new_tab_hover_fg = "#16161e",
+}
+
+return colors
